1ce7d83bf2972d490b003ecb1d6ed6fbb5b7c7fa
Arahabica <r.sasage@gmail.com>
2021-09-20 23:31:22 +0900

    add API [GET] /me"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  skip_before_action :verify_authenticity_token, if: :devise_controller? # skif CSRF check if API
+  before_action :authenticate_user!, unless: :devise_controller?
+
+  skip_before_action :verify_authenticity_token, if: :devise_controller? # skip CSRF check if API
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < ApplicationController
+    def me
+      render json: current_user, status: :ok
+    end
+  end
